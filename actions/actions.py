225071114ffa8576144e5bde63d57c8211b6bc05
yangchihung <young900221@gmail.com>
2022-07-10 18:20:20 +0800

    0710

--- actions/actions.py
+++ actions/actions.py
@@ -9,8 +9,8 @@
 
 from typing import Any, Text, Dict, List
 
-from rasa_core_sdk import Action, Tracker
-from rasa_core_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 import pymysql
 
 
@@ -24,45 +24,52 @@ class QueryResourceType(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         # conn = DbQueryMethods.create_connection(db_file="D://User//Rasa//edu_db//venueDB.db")
-        db_settings = {
-            "host": "127.0.0.1",
-            "port": 3306,
-            "user": "root",
-            "password": "pisces900221",
-            "db": "PeopleFlowDB",
-            "charset": "utf8"
-        }
+        db_settings = { "host": "140.83.85.111",
+                        "port": 3306,
+                        "user": "admin",
+                        "password": "@050610AIoT",
+                        "db": "test_database",
+                        "charset": "utf8"}
         conn = pymysql.connect(**db_settings)
-
-        slot_value = tracker.get_slot("resource_type")
-        get_query_results = DbQueryMethods.select_by_slot(conn=conn, slot_value=slot_value)
-        dispatcher.utter_message(text=str(get_query_results))
-
-        return []
-
-class DbQueryMethods:
-    # def create_connection(db_file):
-    #     """ create a database connection to the SQLite database
-    #         specified by the db_file
-    #     :param db_file: database file
-    #     :return: Connection object or None
-    #     """
-    #     conn = None
-    #     try:
-    #         conn = sqlite3.connect(db_file)
-    #     except Error as e:
-    #         print(e)
-
-    #     return conn
-
-    def select_by_slot(conn, slot_value):
 
         try:
             with conn.cursor() as cursor:
 
-                command = "SELECT * FROM "+ slot_value + ";"
-                cursor.execute(command)
-                result = cursor.fetchone()
-                return result
+                cursor.execute("select * from Stadium ORDER BY set_time")
+                rows = cursor.fetchall()
+
+                for row in rows:
+                    result = f"The following is the information:\npeople flow:{row[1]}\ntemp:{row[2]}\nhumidity:{row[3]}\nair_quality:{row[4]}"
         except Exception as ex:
-            return ex
+            print(ex)
+
+        dispatcher.utter_message(text=str(result))
+
+        return []
+
+# class DbQueryMethods:
+#     # def create_connection(db_file):
+#     #     """ create a database connection to the SQLite database
+#     #         specified by the db_file
+#     #     :param db_file: database file
+#     #     :return: Connection object or None
+#     #     """
+#     #     conn = None
+#     #     try:
+#     #         conn = sqlite3.connect(db_file)
+#     #     except Error as e:
+#     #         print(e)
+
+#     #     return conn
+
+#     def select_by_slot(conn, slot_value):
+
+#         try:
+#             with conn.cursor() as cursor:
+
+#                 # command = "SELECT * FROM "+ test_table + ";"
+#                 cursor.execute("SELECT * FROM "+ slot_value + ";")
+#                 result = cursor.fetchone()
+#                 return result
+#         except Exception as ex:
+#             return ex
